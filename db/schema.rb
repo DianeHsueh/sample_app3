8fdbf07be45cc612cbe84fa0df1e8baa848d3a02
DianeHsueh <dia19910403@gmail.com>
2015-03-03 19:55:15 +0800

    add count activation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214124155) do
+ActiveRecord::Schema.define(version: 20150228073753) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20150214124155) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin",           default: false
+    t.boolean  "activated",       default: false
+    t.datetime "activated_at"
+    t.string   "reset_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
