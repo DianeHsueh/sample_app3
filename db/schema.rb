34dfbfb49538a5bbbaca3d3b50852069c9d44833
DianeHsueh <dia19910403@gmail.com>
2015-03-03 20:07:37 +0800

    chapter10

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150228073753) do
     t.boolean  "activated",       default: false
     t.datetime "activated_at"
     t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
